2016c480cc55bae81ecf596af53ce2f9af0a672a
Parthav <parthavsabrwal@gmail.com>
2024-12-21 11:36:08 +0530

    update config.js and added config.js in git ignore

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://2bf4-2401-4900-1c35-409b-991e-189-fa37-adcb.ngrok-free.app/';
+export const BASE_URL = 'https://adef-2401-4900-1c34-5fc1-6d67-4f97-407d-c1a7.ngrok-free.app';
 
 // export const BASE_URL = 'http://localhost:8000/';
 
