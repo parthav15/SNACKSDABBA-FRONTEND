0ca3b49923c13f30375697206354515b83fdc8fc
Parthav <parthavsabrwal@gmail.com>
2024-12-30 23:36:38 +0530

    Updated Config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://8b35-2401-4900-1c34-31fc-4575-6a0f-f527-b9a3.ngrok-free.app/';
+export const BASE_URL = 'https://d7ba-2401-4900-1c73-3dc-1459-bf6-76e7-baf7.ngrok-free.app/';
 
 // export const BASE_URL = 'http://localhost:8000/';
 
