ddf89e262f805c465c15bd6fb681c09204832d33
Parthav <parthavsabrwal@gmail.com>
2024-12-31 19:08:32 +0530

    Added wishlist icon for the product cards

--- src/layouts/frontendProductCard/index.js
+++ src/layouts/frontendProductCard/index.js
@@ -5,6 +5,7 @@ import { BASE_URL } from "config";
 const ProductCard = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [wishlist, setWishlist] = useState({});
 
   const handleBuyClick = (productId) => {
     setCart((prevCart) => ({ ...prevCart, [productId]: true }));
@@ -12,6 +13,13 @@ const ProductCard = () => {
 
   const handleRemoveClick = (productId) => {
     setCart((prevCart) => ({ ...prevCart, [productId]: false }));
+  };
+
+  const handleWishlistClick = (productId) => {
+    setWishlist((prevWishlist) => ({
+      ...prevWishlist,
+      [productId]: !prevWishlist[productId],
+    }));
   };
 
   useEffect(() => {
@@ -36,54 +44,60 @@ const ProductCard = () => {
 
   return (
     <>
-    <div className="text-center mb-6">
+      <div className="text-center mb-6">
         <h1 className="text-4xl font-bold text-gray-900 inline-block relative">
-            <span className="text-rose-500">Featured</span> Products
-            <span className="absolute left-0 bottom-0 w-full border-b-2 border-rose-500"></span>
+          <span className="text-rose-500">Featured</span> Products
+          <span className="absolute left-0 bottom-0 w-full border-b-2 border-rose-500"></span>
         </h1>
         <p className="text-sm text-gray-600">
-            Our most popular products, handpicked by our team.
+          Our most popular products, handpicked by our team.
         </p>
-    </div>
-    <div className="wrapper">
-      {products.map((product) => (
-        <div className="container" key={product.id}>
-          <div className="top">
-            <img
-              src={`${BASE_URL}media/${product.image[0]}`}
-              alt={product.name}
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          </div>
-          <div className={`bottom ${cart[product.id] ? "clicked" : ""}`}>
-            <div className="left">
-              <div className="details">
-                <h1 className="whitespace-nowrap">{product.name}</h1>
-                <p>Rs. {product.price}</p>
-              </div>
-              <div className="buy" onClick={() => handleBuyClick(product.id)}>
-                <i className="material-icons">add_shopping_cart</i>
+      </div>
+      <div className="wrapper">
+        {products.map((product) => (
+          <div className="container" key={product.id}>
+            <div className="top">
+              <img
+                src={`${BASE_URL}media/${product.image[0]}`}
+                alt={product.name}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+              <div
+                className={`wishlist ${wishlist[product.id] ? "added" : ""}`}
+                onClick={() => handleWishlistClick(product.id)}
+              >
+                <i className="material-icons">favorite_border</i>
               </div>
             </div>
-            <div className="right">
-              <div className="done">
-                <i className="material-icons">done</i>
+            <div className={`bottom ${cart[product.id] ? "clicked" : ""}`}>
+              <div className="left">
+                <div className="details">
+                  <h1 className="whitespace-nowrap">{product.name}</h1>
+                  <p>Rs. {product.price}</p>
+                </div>
+                <div className="buy" onClick={() => handleBuyClick(product.id)}>
+                  <i className="material-icons">add_shopping_cart</i>
+                </div>
               </div>
-              <div className="details">
-                <h1>Added to Cart</h1>
-              </div>
-              <div className="remove" onClick={() => handleRemoveClick(product.id)}>
-                <i className="material-icons">clear</i>
+              <div className="right">
+                <div className="done">
+                  <i className="material-icons">done</i>
+                </div>
+                <div className="details">
+                  <h1>Added to Cart</h1>
+                </div>
+                <div className="remove" onClick={() => handleRemoveClick(product.id)}>
+                  <i className="material-icons">clear</i>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </>
   );
 };
