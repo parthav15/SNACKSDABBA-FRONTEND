6db93e2cdae38296aadd58cd948fb5dd52d4b57e
parthav15 <parthavsabrwal@gmail.com>
2025-03-04 06:43:11 +0530

    Latest

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-export const BASE_URL = 'https://snacksdabba.pythonanywhere.com/';
+export const BASE_URL = 'https://842f-100-27-21-231.ngrok-free.app/';
 
 // export const BASE_URL = 'http://localhost:8000/';
-
