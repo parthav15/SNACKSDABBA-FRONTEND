0388c3dd0d702cd6cb12aabd43d8aba28627c808
= <=>
2025-03-09 04:59:33 +0530

    Updated ngrok url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://747f-100-27-21-231.ngrok-free.app/snacksdabba/';
+export const BASE_URL = 'https://6094-44-203-250-65.ngrok-free.app/snacksdabba/';
 
 // export const BASE_URL = 'http://localhost:8000/';
